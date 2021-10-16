83fe6f7134e7bea511b6d98247073314323cf958
Qudae <qudaeo@yandex.ru>
2021-10-16 13:31:21 +0700

    fix 1

--- App.js
+++ App.js
@@ -25,32 +25,9 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Gallery from "./components/Gallery";
+import {Provider} from "mobx-react/src/Provider";
+//import store from "./mobx/store";
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -65,31 +42,17 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <Text>***</Text>
+        <Gallery></Gallery>
+
+
       </ScrollView>
     </SafeAreaView>
+
   );
 };
 
+/*
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
@@ -108,5 +71,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
+ */
 
 export default App;
---
+++ components/Gallery.js
@@ -0,0 +1,9 @@
+import React from "react";
+//import { inject } from "mobx-react";
+import {Text} from "react-native";
+
+const Gallery = () => {
+    return <Text>Hello World!</Text>;
+};
+
+export default Gallery;
