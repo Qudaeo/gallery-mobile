07d0bf048c98f1ef8dde2daf5350c32e96dd04fe
Qudae <qudaeo@yandex.ru>
2021-10-17 11:29:21 +0700

    add store

--- App.js
+++ App.js
@@ -7,51 +7,54 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+//import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import Gallery from "./components/Gallery";
-import {Provider} from "mobx-react/src/Provider";
+import {Provider} from "mobx-react";
 import store from "./mobx/store";
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  return (
-      <Provider store={store}>
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Text>***</Text>
-        <Gallery/>
+    return (
+        <Provider {...store}>
+
+            <SafeAreaView style={backgroundStyle}>
+                <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={backgroundStyle}>
+                    <Text>***</Text>
+                    <Gallery/>
+                    <Text>***</Text>
 
 
-      </ScrollView>
-    </SafeAreaView>
-      </Provider>
+                </ScrollView>
+            </SafeAreaView>
+        </Provider>
 
-  );
+
+    );
 };
 
 /*
@@ -75,4 +78,4 @@ const styles = StyleSheet.create({
 });
  */
 
-export default App;
+export default () => <Provider appState={store}><App/></Provider>;
--- components/Gallery.js
+++ components/Gallery.js
@@ -1,9 +1,36 @@
-import React from "react";
-//import { inject } from "mobx-react";
-import {Text} from "react-native";
+import React, {useEffect} from "react";
+import {inject} from "mobx-react";
+import {Image, Text, View, StyleSheet} from "react-native";
+import {decorate} from "core-decorators";
+import {observable} from "mobx";
 
-const Gallery = () => {
-    return <Text>Hello World!</Text>;
+const Gallery = (props) => {
+    useEffect(() => {
+
+    }, [])
+
+    return <Text>{props.gallery.author}</Text>
 };
 
-export default Gallery;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000',
+    },
+    image: {
+        flex: 1,
+        alignSelf: 'stretch',
+        height: null,
+        width: null,
+        backgroundColor: '#000',
+    }
+});
+
+//export default Gallery;
+export default inject("gallery")(Gallery);
+
+decorate(Gallery, {
+    gallery: observable
+})
