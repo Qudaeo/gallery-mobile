7a4f19437f4daa023cb56d30d092bdc026e3c41f
qudaeo <qudaeo@yandex.ru>
2021-10-19 10:45:15 +0700

    main App.js refactoring

--- App.js
+++ App.js
@@ -7,47 +7,15 @@
  */
 
 import React from 'react';
-import {StatusBar} from 'react-native';
-
 
 import Gallery from "./components/Gallery";
 import {Provider} from "mobx-react";
 import Store from "./mobx/store";
-import {isDarkMode} from "./common/colorSchemeStyles";
 
 const App = () => {
-    return (<Provider {...Store}>
-            {/*<SafeAreaView style={backgroundStyle}>*/}
-            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
-
-            <Gallery/>
-
-            {/*</SafeAreaView>*/}
-        </Provider>
-    );
+    return <Provider {...Store}>
+        <Gallery/>
+    </Provider>
 };
 
 export default App
-
-/*
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-})
- */
-
-
--- common/colorSchemeStyles.js
+++
@@ -1,8 +0,0 @@
-import {useColorScheme} from "react-native";
-import {Colors} from "react-native/Libraries/NewAppScreen";
-
-export const isDarkMode = useColorScheme() === 'dark'
-
-export const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-}
