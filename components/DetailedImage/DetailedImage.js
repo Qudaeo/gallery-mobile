6428830d2ed307e8f916973c792953b76e6cf9ce
Qudae <qudaeo@yandex.ru>
2021-10-21 21:21:52 +0700

    add fetching on open DetailedImage.js

--- components/DetailedImage/DetailedImage.js
+++ components/DetailedImage/DetailedImage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Image, ScrollView, TouchableOpacity, useWindowDimensions, View} from "react-native";
 import {observer} from "mobx-react";
 import {actionsPictures, marginHorizontal, marginVertical} from "../../common/const"
@@ -6,12 +6,15 @@ import {calcImageDimensions} from "../../common/funcions"
 import {useStore} from "../../mobx/store";
 import PhotoAction from "./PhotoAction";
 
-
-
 const DetailedImage = () => {
     const {galleryStore} = useStore()
 
     const photo = galleryStore.detailPhoto
+
+    useEffect(() => {
+        galleryStore.getGalleryImage(photo.id, photo.width, photo.height)
+    }, [galleryStore.base64Images[photo.id]])
+
 
     const photoDimensions = calcImageDimensions(useWindowDimensions().width, photo.height / photo.width, 1)
 
@@ -34,7 +37,7 @@ const DetailedImage = () => {
         </TouchableOpacity>
 
         <View style={{flex: 1, flexDirection: 'row', marginTop: 15}}>
-            {actionsPictures.map((el, index) => <PhotoAction  key={index} index={index}/>)}
+            {actionsPictures.map((el, index) => <PhotoAction key={index} index={index}/>)}
         </View>
 
     </ScrollView>
