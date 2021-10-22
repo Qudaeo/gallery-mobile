49826b8636ddf944042c82bc9fc494bf3dd2d12d
qudaeo <qudaeo@yandex.ru>
2021-10-22 09:30:52 +0700

    refactor GalleryRow.js & GalleryItem

--- components/Gallery/GalleryItem.js
+++ components/Gallery/GalleryItem.js
@@ -1,7 +1,5 @@
 import React from "react";
-import {Image, Text, TouchableOpacity, useWindowDimensions, View} from "react-native";
-import {marginHorizontal} from "../../common/const";
-import {calcImageDimensions} from "../../common/funcions";
+import {Image, Text, TouchableOpacity} from "react-native";
 import {observer} from "mobx-react";
 import {useStore} from "../../mobx/store";
 
@@ -9,52 +7,38 @@ export const GalleryItem = (props) => {
 
     const {galleryStore} = useStore()
 
-    const ratio = props.image.height / props.image.width
+    const id = props.image.id
+    const width = props.imageDimensions.width
+    const height = props.imageDimensions.height
 
-    const imageDimensions = calcImageDimensions(useWindowDimensions().width, ratio, galleryStore.appColumnCount)
+
 
     const openDetailedImage = () => {
-        galleryStore.setDetailPhoto(props.image.id, imageDimensions.width, imageDimensions.height)
+        galleryStore.setDetailPhoto(id, width, height)
         props.navigation.navigate('DetailedImage')
     };
 
-    return <View style={{
-        marginLeft: marginHorizontal,
-        width: imageDimensions.width,
-        height: imageDimensions.height,
-    }}>
+    return <TouchableOpacity activeOpacity={.7} onPress={() => openDetailedImage()}>
 
+        <Image
+            style={{
+                width: width,
+                height: height,
+                position: "relative",
+            }}
+            source={{uri: galleryStore.base64Images[id]}}
+        />
 
-        {/*<Text>{`${props.image.id} - ${props.image.width}:${props.image.height} - ${imageDimensions.width}:${imageDimensions.height}`}</Text>*/}
-        {/*<Text>{`${props.rowRatio}`}</Text>*/}
+        <Text style={{
+            position: 'absolute',
+            fontSize: Math.round(12 / galleryStore.appColumnCount),
+            color: 'rgba(255,255,255,0.5)',
+            textAlign: "right",
+            bottom: Math.round(5 / galleryStore.appColumnCount),
+            right: Math.round(10 / galleryStore.appColumnCount),
+        }}>{`Photo by ${props.image.author}`}</Text>
 
-        <TouchableOpacity activeOpacity={.7} onPress={() => openDetailedImage()}>
-
-            <Image
-                style={{
-                    width: imageDimensions.width,
-                    height: imageDimensions.height,
-                    position: "relative",
-                }}
-                source={{uri: galleryStore.base64Images[props.image.id]}}
-            />
-
-
-            {/* <Text>{props.galleryStore.images[props.image.id]}</Text>*/}
-
-
-            <Text style={{
-                position: 'absolute',
-                fontSize: Math.round(12 / galleryStore.appColumnCount),
-                color: 'rgba(255,255,255,0.5)',
-                textAlign: "right",
-                bottom: Math.round(5 / galleryStore.appColumnCount),
-                right: Math.round(10 / galleryStore.appColumnCount),
-            }}>{`Photo by ${props.image.author}`}</Text>
-
-
-        </TouchableOpacity>
-    </View>
+    </TouchableOpacity>
 }
 
 export default observer(GalleryItem)
--- components/Gallery/GalleryRow.js
+++ components/Gallery/GalleryRow.js
@@ -1,25 +1,32 @@
 import React from "react";
-import {useWindowDimensions, View} from "react-native";
+import {Text, useWindowDimensions, View} from "react-native";
 import GalleryItem from "./GalleryItem";
-import {marginVertical} from "../../common/const";
+import {marginHorizontal, marginVertical} from "../../common/const";
 import {calcImageDimensions} from "../../common/funcions";
 
 
 export const GalleryRow = (props) => {
 
-    const imageDimensions = calcImageDimensions(useWindowDimensions().width, props.row[0].height / props.row[0].width, props.row[0].length)
+    const appWidth = useWindowDimensions().width
+
+    const imageDimensions = props.row.map(image =>
+        calcImageDimensions(appWidth, image.height / image.width, props.row.length))
+
+    //   const rowImageRatio = props.row.map(image => imageDimensions.width / (image.width + image.height))
 
     return <View style={[{
         flexDirection: 'row',
         //       direction: 'row',
         marginVertical: marginVertical,
     }]}>
-        {props.row.map(el => <View key={el.id} style={{
-                //width: Math.round( 1 * 100) + '%'
+        {/* <Text>{JSON.stringify(imageDimensions)}</Text>*/}
+        {props.row.map((el, index) => <View key={el.id} style={{
+                width: imageDimensions[index].width,
+                height: imageDimensions[index].height,
+                marginLeft: marginHorizontal,
             }}>
-
                 <GalleryItem image={el}
-                             navigation={props.navigation}/>
+                             navigation={props.navigation} imageDimensions={imageDimensions[index]}/>
             </View>
         )}
     </View>
