0531389b51f5ae5ef83ab42273f621c06ab33f82
qudaeo <qudaeo@yandex.ru>
2021-10-28 10:59:57 +0700

    fix SearchPhotoBar.js

--- components/GalleryScreen/SearchPhotoBar.js
+++ components/GalleryScreen/SearchPhotoBar.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
-import {StyleSheet} from "react-native";
+import React, {useEffect, useRef, useState} from "react";
+import {StyleSheet, useWindowDimensions} from "react-native";
 import {Searchbar} from 'react-native-paper';
 import magnifierPicture from '../../images/GalleryScreen/magnifier.png'
 import clearPicture from '../../images/GalleryScreen/cancel.png'
 
 const SearchPhotoBar = (props) => {
+
     const [isFocused, setIsFocused] = useState(false);
+
+    const currentWindowWidth = useWindowDimensions().width
+
+    const searchbarRef = useRef(null);
+    useEffect(() => {
+        if (isFocused) {
+            searchbarRef.current.focus()
+        }
+    }, [isFocused])
 
     const styles = StyleSheet.create({
         searchButton: {
@@ -13,22 +23,25 @@ const SearchPhotoBar = (props) => {
             left: 15,
             top: 10,
             zIndex: 100,
-            width: isFocused ? 300 : 50,
+            width: isFocused ? currentWindowWidth - 100 : 50,
             borderRadius: 50,
             height: 50,
 
-            backgroundColor: "rgba(230, 249, 255, 0.7)",
+            backgroundColor: isFocused ? "rgba(230, 249, 255, 0.92)" : "rgba(230, 249, 255, 0.7)",
         }
     });
 
     return <Searchbar
+        ref={searchbarRef}
         style={styles.searchButton}
         placeholder="Search photos..."
         onChangeText={text => {
             props.searchTextChange(text)
         }}
         value={props.searchText}
-        onIconPress={() => setIsFocused(!isFocused)}
+        onIconPress={() => {
+            setIsFocused(!isFocused)
+        }}
         onBlur={() => setIsFocused(false)}
 
         icon={magnifierPicture}
