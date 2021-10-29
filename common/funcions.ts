2e938e8bf42914a8ba9cbbbd1e826720991f9d7b
Qudae <qudaeo@yandex.ru>
2021-10-30 04:07:29 +0700

    convert functions to ts

--- common/funcions.ts
+++ common/funcions.ts
@@ -1,6 +1,6 @@
 import {marginHorizontal} from "./const";
 
-export const calcImageDimensions = (oldWidth, oldHeight, columnCount = 1) => {
+export const calcImageDimensions = (oldWidth: number, oldHeight: number, columnCount = 1) => {
 
     const newWidth = Math.round((oldWidth - 2 * marginHorizontal - (columnCount - 1) * marginHorizontal) / columnCount)
 
