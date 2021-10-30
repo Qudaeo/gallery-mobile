e068d8558278d4f0b085861f94a590c2aae47890
Qudae <qudaeo@yandex.ru>
2021-10-30 13:57:50 +0700

    convert PhotoAction to ts

---
+++ components/DetailedImageScreen/PhotoAction.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Image, ImageSourcePropType, TouchableOpacity} from "react-native";
+import {actionsPictures} from "../../common/const";
+
+type IProps = {
+    index: number
+}
+
+const PhotoAction: React.FC<IProps> = ({index}) => <TouchableOpacity activeOpacity={.5}>
+
+    <Image
+        style={{
+            width: 30,
+            height: 30,
+            marginLeft: 15
+        }}
+        source={actionsPictures[index] as ImageSourcePropType}/>
+
+</TouchableOpacity>
+
+export default PhotoAction
