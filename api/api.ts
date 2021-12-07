36a851ed6ee0830011e9285cf8d939bebff976f7
Qudaeo <qudaeo@yandex.ru>
2021-12-07 13:41:05 +0700

    downgrade eslint & remove prettier

--- api/api.ts
+++ api/api.ts
@@ -1,49 +1,49 @@
-import axios from "axios";
-import {API_KEY, baseURL} from "../common/const";
-import {DetailsType, PhotoType} from "../mobx/GalleryStore";
+import axios from 'axios';
+import { API_KEY, baseURL } from '../common/const';
+import { DetailsType, PhotoType } from '../mobx/GalleryStore';
 
 const instance = axios.create({
-    baseURL,
-    headers: {"Authorization": `Client-ID ${API_KEY}`}
-})
+  baseURL,
+  headers: { 'Authorization': `Client-ID ${API_KEY}` }
+});
 
 type GetImageResponseType = {
-    headers: {
-        'content-type': string
-    }
-    data: ArrayBuffer
+  headers: {
+    'content-type': string
+  }
+  data: ArrayBuffer
 }
 
 export const galleryAPI = {
-    async getGallery(currentPage: number, pageSize: number): Promise<{ data: PhotoType[] } | undefined> {
-        try {
-            return instance.get(`/photos?page=${currentPage}&per_page=${pageSize}`)
-        } catch (e) {
-            alert('Exception: galleryAPI: getGallery(currentPage, pageSize): ' + e.message)
-        }
-    },
+  async getGallery(currentPage: number, pageSize: number) {
+    try {
+      return instance.get<PhotoType[]>(`/photos?page=${currentPage}&per_page=${pageSize}`);
+    } catch (e) {
+      alert('Exception: galleryAPI: getGallery(currentPage, pageSize): ' + e.message);
+    }
+  },
 
-    async getSearchedGallery(searchText: string, currentPage: number, pageSize: number): Promise<{ data: { results: PhotoType[] } } | undefined> {
-        try {
-            return instance.get(`/search/photos?query=${searchText}&page=${currentPage}&per_page=${pageSize}`)
-        } catch (e) {
-            alert('Exception: galleryAPI: getSearchedGallery(searchText, currentPage, pageSize): ' + e.message)
-        }
-    },
+  async getSearchedGallery(searchText: string, currentPage: number, pageSize: number) {
+    try {
+      return instance.get<{ results: PhotoType[] }>(`/search/photos?query=${searchText}&page=${currentPage}&per_page=${pageSize}`);
+    } catch (e) {
+      alert('Exception: galleryAPI: getSearchedGallery(searchText, currentPage, pageSize): ' + e.message);
+    }
+  },
 
-    async getImageByUrl(url: string, width?: number, height?: number): Promise<GetImageResponseType | undefined> {
-        try {
-            return instance.get(`${url}${(width || height)?'?':''}${width?`w=${width}`:''}${height?`&h=${height}`:''}`, {responseType: 'arraybuffer'})
-        } catch (e) {
-            alert('Exception: galleryAPI: getImageByUrl(url, width, height): ' + e.message)
-        }
-    },
+  async getImageByUrl(url: string, width?: number, height?: number): Promise<GetImageResponseType | undefined> {
+    try {
+      return instance.get(`${url}${(width || height) ? "?" : ""}${width ? `w=${width}` : ""}${height ? `&h=${height}` : ""}`, { responseType: "arraybuffer" });
+    } catch (e) {
+      alert('Exception: galleryAPI: getImageByUrl(url, width, height): ' + e.message);
+    }
+  },
 
-    async getPhotoDetail(id: string): Promise<{ data: DetailsType } | undefined> {
-        try {
-            return instance.get(`/photos/${id}`)
-        } catch (e) {
-            alert('Exception: galleryAPI: getPhotoDetail(id): ' + e.message)
-        }
-    },
-}
+  async getPhotoDetail(id: string) {
+    try {
+      return instance.get<DetailsType>(`/photos/${id}`);
+    } catch (e) {
+      alert('Exception: galleryAPI: getPhotoDetail(id): ' + e.message);
+    }
+  }
+};
