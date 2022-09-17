464032998a620893baaa8d096cd8079418733aff
Aleksey Pekhterev <qudaeo@yandex.ru>
2022-09-17 12:46:39 +0700

    change project name

--- ios/galleryTests/galleryTests.m
+++ ios/galleryTests/galleryTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ingiTests : XCTestCase
+@interface galleryTests : XCTestCase
 
 @end
 
-@implementation ingiTests
+@implementation galleryTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
